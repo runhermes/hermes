e8670a4e5650c2533ab23f0702cedbd36b8b0ce1
renehernandez <rene.hernandez@fullscript.com>
2020-10-04 15:11:32 -0400

    Update camper to v0.0.7. Log request

--- lib/controller.rb
+++ lib/controller.rb
@@ -14,6 +14,7 @@ class Controller
 
   def process_request
     # TODO: Handle different MR states, open, close or update
+    puts "Request:\n #{@request}"
     resources = @basecamp.resources(@request["object_attributes"]["description"])
 
     return unless resources
