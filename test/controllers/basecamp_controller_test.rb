21cf8adeae451a8d0a5074decffb236a1d2de3fe
renehernandez <renehr9102@protonmail.com>
2020-10-26 13:00:22 -0400

    Remove tests

--- test/controllers/basecamp_controller_test.rb
+++ test/controllers/basecamp_controller_test.rb
@@ -1,14 +1,5 @@
 require 'test_helper'
 
 class BasecampControllerTest < ActionDispatch::IntegrationTest
-  test "should get oauth" do
-    get basecamp_oauth_url
-    assert_response :success
-  end
-
-  test "should get callback" do
-    get basecamp_callback_url
-    assert_response :success
-  end
 
 end
