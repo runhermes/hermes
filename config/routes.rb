84133adbcd9f1a3a57b3fb881a95973da2c969a1
renehernandez <renehr9102@protonmail.com>
2020-10-26 14:42:02 -0400

    Add missing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'basecamp/oauth'
+  get 'basecamp/callback'
+
+  post '/gitlab/webhook', action: :webhook, controller: :gitlab
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
